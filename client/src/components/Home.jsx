2d67083c8beacb093b85164bdb0b0b490ecff836
laurnen <68237271+laurnen@users.noreply.github.com>
2024-03-28 10:51:22 -0500

    Homepage

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,6 +1,25 @@
+import Header from "./components/Header";
+import MapCard from "./components/MapCard";
+import Filter from "./components/Filter";
+import CreateEvent from "./components/CreateEvent";
+import "./assets/home.css";
+
 export default function Home()
 {
     return (
-        <div>[TODO: Insert homepage here]</div>
+        <>
+      <Header />
+      <div className="body">
+        <div className="sidebar">
+          <CreateEvent />
+          <div className="small-space"></div>
+          <Filter />
+        </div>
+        <div className="space"></div>
+        <div className="map">
+          <MapCard />
+        </div>
+      </div>
+    </>
     );
 }
