ea9eca438b05df53322ec0837e12609b1a2c470e
laurnen <68237271+laurnen@users.noreply.github.com>
2024-03-30 10:55:21 -0500

    Update Home.jsx

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -8,7 +8,7 @@ export default function Home()
 {
     return (
         <>
-      <Header />
+      {/* <Header /> */}
       <div className="body">
         <div className="sidebar">
           <CreateEvent />
