fed94fe5d610094a0ccc07ab53549f41befb69a3
adedmla <a.adejumobi67@gmail.com>
2024-03-29 21:29:02 -0500

    CSS changes

--- client/src/components/SignUp.jsx
+++ client/src/components/SignUp.jsx
@@ -1,57 +1,40 @@
-import '../App.css'; 
-const SignUp = () => {
+import React, { useState } from 'react';
+
+import email_icon from '../assets/email.png';
+import password_icon from '../assets/password.png';
+
+const LoginSignup = () => {
+    const [action, setAction] = useState("Sign Up");
+
     return (
-        <div className="App">
-        <body>
-          <div>
-            <h1 className="purple-text">CatNip</h1>
-            <h2>INSERT LOGO HERE</h2>
-          </div>
-          <div>
-            <p>
-              <form action="">
-                <label for="username">Email</label>
-                <br />
-                <input
-                  type="text"
-                  id="username"
-                  placeholder="Your email"
-                  name="username"
-                  required
-                />
-  
-                <br />
-                <label for="password">Password</label>
-                <br />
-                <input
-                  type="text"
-                  id="password"
-                  placeholder="Your password"
-                  name="password"
-                  required
-                />
-                <br />
-                <label for="confirm password">Confirm Password</label>
-                <br />
-                <input
-                  type="text"
-                  id="confirm password"
-                  placeholder="Confirm your password"
-                  name="confirm password"
-                  required
-                />
-                <br />
-                <button type="submit" className="custom-button">
-                  Sign Up
-                </button>
-              </form>
-              Already have an account? <a href="">Login in!</a>
-            </p>
-          </div>
-        </body>
-      </div>
-    );
-  }
-  
-  
-  export default SignUp;
+        <div className='container'>
+            <div className="header">
+                <div className="text">{action}</div>
+                <div className="underline"></div>
+            </div>
+            <div className="inputs">
+                <div className="input">
+                    <img src={email_icon} alt="" />
+                    <input type="email" placeholder="Email" />
+                </div>
+                <div className="input">
+                    <img src={password_icon} alt="" />
+                    <input type="password" placeholder="Password" />
+                </div>
+                {action === "Sign Up" ? 
+                    <div className="input">
+                        <img src={password_icon} alt="" />
+                        <input type="password" placeholder="Confirm Password" />
+                    </div> : 
+                    <div className="forgot-password">Forgot Password? <span>Click Here!</span></div>
+                }
+
+                <div className="submit-container">
+                    <div className="submit" onClick={() => { setAction("Sign Up") }}>Sign Up</div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default LoginSignup;
