aa7f3fff35fe07d9c7451b07c4887d3be7937f22
Elysia Lopez <elysialopez528@gmail.com>
2024-03-30 02:37:47 -0500

    fixed merge errors hehe

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,4 +1,3 @@
-// import React, { useState } from 'react';
 import {
     //   Collapse,
       Navbar,
@@ -15,54 +14,6 @@ import {
     } from 'reactstrap';
     
     //import 'bootstrap/dist/css/bootstrap.min.css'; 
-    
-    function Header(args) {
-    //   const [isOpen, setIsOpen] = useState(false);
-    
-    //   const toggle = () => setIsOpen(!isOpen);
-    
-      return (
-        <div>
-          <Navbar color="light">
-            <NavbarBrand href="/">catnip.nu</NavbarBrand>
-              <Nav tabs >
-                <NavItem>
-                  <NavLink href="https://github.com/reactstrap/reactstrap">About Us</NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink href="https://github.com/reactstrap/reactstrap">
-                    Account
-                  </NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink href="https://github.com/reactstrap/reactstrap">
-                    Preferences
-                  </NavLink>
-                </NavItem>
-              </Nav>
-          </Navbar>
-        </div>
-      );
-    }
-    
-    export default Header;
-// import React, { useState } from 'react';
-import {
-    //   Collapse,
-      Navbar,
-    //   NavbarToggler,
-      NavbarBrand,
-      Nav,
-      NavItem,
-      NavLink,
-    //   UncontrolledDropdown,
-    //   DropdownToggle,
-    //   DropdownMenu,
-    //   DropdownItem,
-    //   NavbarText,
-    } from 'reactstrap';
-    
-    import 'bootstrap/dist/css/bootstrap.min.css'; 
     
     function Header(args) {
     //   const [isOpen, setIsOpen] = useState(false);
