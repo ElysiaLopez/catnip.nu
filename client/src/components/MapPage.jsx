7b0dd898ea7d08b3c3c4a2842eb521b82809ac06
laurnen <68237271+laurnen@users.noreply.github.com>
2024-04-09 00:11:18 -0500

    Implemented Leaflet

--- client/src/components/MapPage.jsx
+++ client/src/components/MapPage.jsx
@@ -1,4 +1,4 @@
-import MapCard from "./MapCard";
+import Map from "./Map";
 import Filter from "./Filter";
 import CreateEvent from "./CreateEvent";
 import "../assets/home.css";
@@ -15,8 +15,8 @@ export default function MapPage()
           <Filter />
         </div>
         <div className="space"></div>
-        <div className="map">
-          <MapCard />
+        <div className="map bg-white h-128 w-64 overflow-hidden rounded border-white border-4 shadow-lg">
+          <Map />
         </div>
       </div>
     </>
