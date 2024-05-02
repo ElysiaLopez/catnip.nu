6044c234d362a87c51aac883db018740de3f75aa
laurnen <68237271+laurnen@users.noreply.github.com>
2024-05-02 02:03:03 -0500

    Updated

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -9,7 +9,7 @@ const Map = () => {
   
     return ( 
       // Make sure you set the height and width of the map container otherwise the map won't show
-        <MapContainer center={[latitude, longitude]} zoom={15} ref={mapRef} className="h-128 w-256">
+        <MapContainer center={[latitude, longitude]} zoom={15} ref={mapRef} className="h-full w-full">
           <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
