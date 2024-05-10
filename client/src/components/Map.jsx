85083c6de483935e35e95676f926fe6009e44961
Elysia Lopez <elysialopez528@gmail.com>
2024-05-09 21:30:43 -0500

    reset

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -2,6 +2,19 @@ import React, { useRef } from "react";
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import "leaflet/dist/leaflet.css";
 
+const Pin = (props) => {
+  return (
+      <Marker position={props.map.position}>
+          <Popup className="max-width-64">
+            <h1 className="font-bold text-xl py-2">{props.map.event}</h1>
+            <h2>{props.map.organization}</h2>
+            <h3>{props.map.tags}</h3>
+            <p>{props.map.description}</p>
+          </Popup>
+        </Marker>
+  );
+}
+
 const Map = () => {
     const mapRef = useRef(null);
     const latitude = 42.054853;
@@ -17,9 +30,11 @@ const Map = () => {
           />
           {/* Additional map layers or components can be added here */}
           <Marker position={Norris}>
-            <Popup>
-              <p className="font-bold">Event At Norris</p>
-              <p>1999 Campus Dr, Evanston, IL 60208</p>
+            <Popup className="max-width-64">
+              <h1 className="font-bold text-xl py-2">Event At Norris</h1>
+              <h2>Organization</h2>
+              <h3>Tags</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
             </Popup>
           </Marker>
         </MapContainer>
