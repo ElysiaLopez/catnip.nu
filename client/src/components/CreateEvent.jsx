6c9326b3541ffadb7ebccc5b2802b30eb00c5ca0
laurnen <68237271+laurnen@users.noreply.github.com>
2024-05-09 21:44:27 -0500

    updated

--- client/src/components/CreateEvent.jsx
+++ client/src/components/CreateEvent.jsx
@@ -1,10 +1,82 @@
-import React, { useState } from 'react';
+import { useState, useEffect } from "react";
+import { useParams, useNavigate } from "react-router-dom";
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter, InputGroup, InputGroupText, Input } from 'reactstrap';
 
 function CreateEvent(args) {
   const [modal, setModal] = useState(false);
 
   const toggle = () => setModal(!modal);
+
+  const [form, setForm] = useState({
+    organization: "",
+    location: "",
+    title: "",
+    tags: "",
+    description: ""
+  });
+
+  const [isNew, setIsNew] = useState(true);
+    const params = useParams();
+    
+    const navigate = useNavigate();
+
+    useEffect(() => {
+      async function fetchData (){
+          const id = params.id?.toString() || undefined;
+
+          if(!id) return;
+
+          setIsNew(false);
+
+          const response = await fetch(`http://localhost:5050/mappins/${params.id.toString()}`);
+
+          if(!response.ok)
+          {
+              const message = `An error has occurred: ${response.statusText}`;
+              console.error(message);
+              return;
+          }
+
+          const record = await response.json();
+
+          if(!record) {
+              console.warn(`Record with id ${id} not found`);
+              toggle;
+              return;
+          }
+
+          setForm(record);
+      }
+      fetchData();
+      return;
+  }, [params.id, toggle]);
+
+// This function will handle the submission.
+async function onSubmit(e) {
+e.preventDefault();
+const pin = { ...form };
+try {
+  let response;
+  if (isNew) {
+    // if we are adding a new record we will POST to /record.
+    response = await fetch("http://localhost:5050/record", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(pin),
+    });
+  }
+
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+} catch (error) {
+  console.error('A problem occurred with your fetch operation: ', error);
+} finally {
+  toggle;
+}
+}
 
   return (
     <div>
@@ -13,51 +85,71 @@ function CreateEvent(args) {
       </button>
       <Modal className="z-10 fixed w-128 m-auto top-4 left-128 bg-white rounded-xl shadow-md p-6" isOpen={modal} toggle={toggle} {...args}>
         <ModalHeader toggle={toggle}><h1 className="text-center font-bold text-xl">Create Event</h1></ModalHeader>
-        <ModalBody >
+        <form onSubmit={onSubmit}>
+        <ModalBody>
         <InputGroup>
                 <InputGroupText>
                 <h2 className="font-bold">Name of Organization </h2>
                 </InputGroupText>
-                <Input className="p-1 w-64 border-purple-200 border-2 rounded-md" />
+                <input type="text"
+                    name="organization"
+                    id="organization"
+                    className="p-1 w-64 border-purple-200 border-2 rounded-md"/>
             </InputGroup>
             <br />
             <InputGroup>
                 <InputGroupText>
                 <h2 className="font-bold">Location </h2>
                 </InputGroupText>
-                <Input className="p-1 w-64 border-purple-200 border-2 rounded-md"/>
+                <input 
+                type="text"
+                name="location"
+                id="location"
+                className="p-1 w-64 border-purple-200 border-2 rounded-md"/>
             </InputGroup>
             <br />
             <InputGroup>
                 <InputGroupText>
                 <h2 className="font-bold">Title of Event </h2>
                 </InputGroupText>
-                <Input className="p-1 w-64 border-purple-200 border-2 rounded-md"/>
+                <input
+                type="text"
+                name="title"
+                id="title"
+                className="p-1 w-64 border-purple-200 border-2 rounded-md"/>
             </InputGroup>
             <br />
             <InputGroup>
                 <InputGroupText>
                 <h2 className="font-bold">Tags </h2>
                 </InputGroupText>
-                <Input className="p-1 w-64 border-purple-200 border-2 rounded-md"/>
+                <input
+                type="text"
+                name="tags"
+                id="tags"
+                 className="p-1 w-64 border-purple-200 border-2 rounded-md"/>
             </InputGroup>
             <br />
             <InputGroup>
                 <InputGroupText>
                 <h2 className="font-bold">Description</h2>
                 </InputGroupText>
-                <textarea className="p-1 h-24 w-96 border-purple-200 border-2 rounded-md"/>
+                <textarea 
+                name="description"
+                id="description"
+                className="p-1 h-24 w-96 border-purple-200 border-2 rounded-md"/>
             </InputGroup>
             <br />
         </ModalBody>
         <ModalFooter>
-          <Button className="p-2 px-4 rounded text-white font-bold bg-purple-300" onClick={toggle}>
+          <Button type="submit" className="p-2 px-4 rounded text-white font-bold bg-purple-300" onClick={toggle}>
             Publish
           </Button>{' '}
           <Button className="p-2 px-4" onClick={toggle}>
             Cancel
           </Button>
         </ModalFooter>
+        </form>
       </Modal>
     </div>
   );
