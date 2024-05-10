026350d9bfb407fd6605a0a366073ced98c2f8bd
Elysia Lopez <elysialopez528@gmail.com>
2024-05-09 23:47:00 -0500

    (temporarily) patched bug

--- client/src/components/CreateEvent.jsx
+++ client/src/components/CreateEvent.jsx
@@ -8,9 +8,9 @@ function CreateEvent(args) {
 
   return (
     <div>
-      <Button className="bg-white bg-opacity-50 rounded-md py-2 px-4" color="primary" onClick={toggle}>
+      <button className="bg-white rounded-lg py-3 px-6 font-bold text-xl shadow-sm hover:bg-purple-300 hover:text-white hover:shadow-lg" color="primary" onClick={toggle}>
         Create Event
-      </Button>
+      </button>
       <Modal className="z-10 fixed w-128 m-auto top-4 left-128 bg-white rounded-xl shadow-md p-6" isOpen={modal} toggle={toggle} {...args}>
         <ModalHeader toggle={toggle}><h1 className="text-center font-bold text-xl">Create Event</h1></ModalHeader>
         <ModalBody >
